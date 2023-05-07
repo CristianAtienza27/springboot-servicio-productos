290eb28ed6c6e9f5cd3b9539460d8b2dbb345c42
Cristian <cristianatienza26@gmail.com>
2023-05-08 01:03:36 +0200

    add: save, update and delete http

--- src/main/java/com/microservices/springbootservicioproductos/controllers/ProductoController.java
+++ src/main/java/com/microservices/springbootservicioproductos/controllers/ProductoController.java
@@ -5,12 +5,11 @@ import com.microservices.springbootservicioproductos.service.IProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class ProductoController {
@@ -30,11 +29,38 @@ public class ProductoController {
     }
 
     @GetMapping("/ver/{id}")
-    public Producto detalle(@PathVariable Long id) {
-        boolean ok = false;
-        if(!ok) {
-            throw new RuntimeException("No se pudo cargar el producto: " + id);
+    public Producto detalle(@PathVariable Long id) throws InterruptedException {
+
+        if (id.equals(10L)) {
+            throw new IllegalStateException("Producto no encontrado");
         }
+        if (id.equals(7L)) {
+            TimeUnit.SECONDS.sleep(5);
+        }
+
         return productoService.findById(id);
+    }
+
+    @PostMapping("/crear")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Producto crear(@RequestBody Producto producto) {
+        return productoService.save(producto);
+    }
+
+    @PutMapping("/editar/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Producto editar(@RequestBody Producto producto, @PathVariable Long id) {
+        Producto productoExist = productoService.findById(id);
+
+        productoExist.setNombre(producto.getNombre());
+        productoExist.setPrecio(producto.getPrecio());
+
+        return productoService.save(productoExist);
+    }
+
+    @DeleteMapping("/eliminar/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void eliminar(@PathVariable Long id) {
+        productoService.deleteById(id);
     }
 }
--- src/main/java/com/microservices/springbootservicioproductos/service/IProductoService.java
+++ src/main/java/com/microservices/springbootservicioproductos/service/IProductoService.java
@@ -8,4 +8,9 @@ public interface IProductoService {
     List<Producto> findAll();
 
     Producto findById(Long id);
+
+    Producto save(Producto producto);
+
+    void deleteById(Long id);
+
 }
